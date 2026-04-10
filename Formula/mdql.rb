ae42115ed00c7a7d6b1c75f416cd8b4a63dff2d3
bliiir <rasgroth@gmail.com>
2026-04-10 17:23:47 +0200

    Add mdql v0.1.0 formula

---
+++ Formula/mdql.rb
@@ -0,0 +1,17 @@
+class Mdql < Formula
+  desc "A database where every entry is a markdown file"
+  homepage "https://github.com/mdql-db/mdql"
+  url "https://github.com/mdql-db/mdql/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "df7f11e540b4263d180c1beb4831f4631c2391da39d1a3f066b7211fa00475f3"
+  license "AGPL-3.0-only"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/mdql")
+  end
+
+  test do
+    assert_match "mdql", shell_output("#{bin}/mdql --help")
+  end
+end
