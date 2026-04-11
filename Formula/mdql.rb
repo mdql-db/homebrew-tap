46ec5fd00b77eb409352742683b20ea03ca4c0a0
bliiir <bliiir@users.noreply.github.com>
2026-04-11 23:11:57 +0200

    Bump mdql to v0.2.2

--- Formula/mdql.rb
+++ Formula/mdql.rb
@@ -1,8 +1,8 @@
 class Mdql < Formula
   desc "A database where every entry is a markdown file"
   homepage "https://github.com/mdql-db/mdql"
-  url "https://github.com/mdql-db/mdql/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "0975584a376254c5bb5b7cd9b2e49fd6b87e50eed296bb829e4098a428b1eb65"
+  url "https://github.com/mdql-db/mdql/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "365caa4c937fdc5853abbb29e8b02385e1285ae9af09f32cb677482d813e1b0a"
   license "AGPL-3.0-only"
 
   depends_on "rust" => :build
