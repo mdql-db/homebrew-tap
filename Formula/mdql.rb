cd4838f4223c081a497446aaf1c7152f883b8547
bliiir <bliiir@users.noreply.github.com>
2026-04-30 18:01:29 +0200

    Update mdql to v0.5.18

--- Formula/mdql.rb
+++ Formula/mdql.rb
@@ -1,8 +1,8 @@
 class Mdql < Formula
   desc "A database where every entry is a markdown file"
   homepage "https://github.com/mdql-db/mdql"
-  url "https://github.com/mdql-db/mdql/archive/refs/tags/v0.5.17.tar.gz"
-  sha256 "98e99b22ba8efd13c375e958b2d5c34d9faf1ac94688bcde1c69f01129a70ef7"
+  url "https://github.com/mdql-db/mdql/archive/refs/tags/v0.5.18.tar.gz"
+  sha256 "038559cf7cb3079eb98ce4e8c4131d08189d33c5c3be0e35d7686d962d3ce91a"
   license "AGPL-3.0-only"
 
   depends_on "rust" => :build
